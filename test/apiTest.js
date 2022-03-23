3c1153e948a66a23779471ec5d1205b899423208
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-03-23 20:41:59 +0100

    Updated Test file

--- test/apiTest.js
+++ test/apiTest.js
@@ -1,8 +1,23 @@
 const request = require("supertest");
 const { server } = require("../index");
 
+describe("GET /", function () {
+  it("respond with 200", function () {
+    request(server).get("/").expect(200);
+  });
+});
 describe("POST /", function () {
   it("respond with 200", function (done) {
-    request(server).post("/").expect(200, done);
+    request(server).post("/create").expect(200, done);
+  });
+});
+describe("PUT /", function () {
+  it("respond with 200", function (done) {
+    request(server).put("/edit").expect(200, done);
+  });
+});
+describe("DELETE /", function () {
+  it("respond with 200", function (done) {
+    request(server).delete("/delete").expect(200, done);
   });
 });
